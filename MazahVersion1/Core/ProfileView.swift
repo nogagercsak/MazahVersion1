e31a69115bbf8640ca011d7796acdd39dab21375
Noga Gercsak <135066149+nogagercsak@users.noreply.github.com>
2023-11-29 20:04:28 -0500

    updated UI and added new views

---
+++ MazahVersion1/Core/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  MazahVersion1
+//
+//  Created by Noga Gercsak on 11/29/23.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
