bedbadb891b1b868a0483343005f7633e17b212e
Ishika <ishika.meel.17@gmail.com>
2024-03-23 12:07:48 -0400

    Map feature

---
+++ MazahVersion1/Core/MapFeature.swift
@@ -0,0 +1,19 @@
+//
+//  MapFeature.swift
+//  MazahVersion1
+//
+//  Created by Ishika Meel on 3/23/24.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapFeature: View {
+    var body: some View {
+        Map()
+    }
+}
+
+#Preview {
+    MapFeature()
+}
