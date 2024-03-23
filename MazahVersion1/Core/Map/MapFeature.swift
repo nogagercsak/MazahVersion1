91907b7789c671266a4f6430f53a9d03cc12f051
Ishika <ishika.meel.17@gmail.com>
2024-03-23 11:57:25 -0400

    map framework

---
+++ MazahVersion1/Core/Map/MapFeature.swift
@@ -0,0 +1,17 @@
+//  Map.swift
+//  MazahVersion1
+//
+//  Created by Ishika Meel on 3/23/24.
+
+import SwiftUI
+import MapKit
+
+struct Map: View {
+    var body: some View {
+        Map()
+    }
+}
+
+#Preview {
+    Map()
+}
